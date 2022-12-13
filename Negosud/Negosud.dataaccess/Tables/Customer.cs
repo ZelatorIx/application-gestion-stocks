3eea2d7b878a5586cbe8e4f415c3b1c45e6b87b9
Clément Cantaloube <clement.cantaloube@gmail.com>
2022-12-13 19:37:02 +0100

    Adding new tables and adding interfaces (ORM)

--- Negosud/Negosud.dataaccess/Tables/Customer.cs
+++ Negosud/Negosud.dataaccess/Tables/Customer.cs
@@ -6,17 +6,16 @@ using System.Threading.Tasks;
 
 namespace Negosud.dataaccess.Tables
 {
-    public class Client
+    public class Customer
     {
         public int Id { get; set; }
         public string FirstName { get; set; } = "";
         public string LastName { get; set; } = "";
-        public int Phone { get; set; }
+        public string Email { get; set; } = "";
+        public int PhoneNumber { get; set; }
         public string PhysicalAddress { get; set; } = "";
-        public string ComplementaryAddress { get; set; } = "";
         public int PostalCode { get; set; }
         public string Town { get; set; } = "";
-        public string ElectronicAddress { get; set; } = "";
         public Item[]? Items { get; set; }
     }
 }
