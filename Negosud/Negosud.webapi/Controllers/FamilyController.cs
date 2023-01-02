ad0e45c6d9cb10a84334ffd1596dc19eb4f4a014
Clément Cantaloube <clement.cantaloube@gmail.com>
2023-01-02 13:20:10 +0100

    Updated

--- Negosud/Negosud.webapi/Controllers/FamilyController.cs
+++ Negosud/Negosud.webapi/Controllers/FamilyController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Negosud.dataaccess;
 using Negosud.dataaccess.Tables;
 using Negosud.webapi.Models;
@@ -9,48 +10,136 @@ namespace Negosud.webapi.Controllers
     [Route("families")]
     public class FamilyController : ControllerBase
     {
+        private readonly NegosudContext _context;
+
+        public FamilyController(NegosudContext context)
+        {
+            _context = context;
+        }
+
+        /**
+         * Retourne une liste de familles
+         * 
+         * @returns Liste de familles
+         */
         [HttpGet]
-        public IEnumerable<FamilyDTO> GetAll()
+        public async Task<List<FamilyDTO>> GetAll()
         {
-            using (NegosudContext context = new NegosudContext())
-            {
-                return context.Families.ToList().Select(f => convert(f)).ToList();
-            }
+            return await _context.Families
+                .Select((Family family) => ConvertToFamilyDTO(family))
+                .ToListAsync();
         }
 
+        /**
+         * Retourne une famille par son identifiant
+         * 
+         * @param id Identifiant de la famille
+         * @return Famille
+         */
         [HttpGet("{id}")]
-        public ActionResult<FamilyDTO> GetById(int id)
+        public async Task<ActionResult<FamilyDTO>> GetById(int id)
         {
-            using (NegosudContext context = new NegosudContext())
+            Family? family = await _context.Families.FindAsync(id);
+
+            if (family == null)
             {
-                Family? family = context.Families.FirstOrDefault(f => f.Id == id);
-                FamilyDTO familyDTO = convert(family);
-                return familyDTO != null ? Ok(familyDTO) : NotFound();
+                return NotFound();
             }
+
+            return Ok(ConvertToFamilyDTO(family));
         }
 
-        private FamilyDTO convert(Family family)
+        /**
+         * Crée une famille
+         * 
+         * @param familyDTO Famille à créer
+         * @return Etat de la requête
+         */
+        [HttpPost]
+        public async Task<ActionResult<FamilyDTO>> Post([FromBody] FamilyDTO familyDTO)
+        {
+            Family familyResult = new Family() { Name = familyDTO.Name };
+
+            _context.Families.Add(familyResult);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(
+                nameof(GetById),
+                new { id = familyDTO.Id },
+                ConvertToFamilyDTO(familyResult)
+            );
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, [FromBody] FamilyDTO familyDTO)
+        {
+            if (id != familyDTO.Id)
+            {
+                return BadRequest();
+            }
+
+            Family? family = await _context.Families.FindAsync(id);
+            if (family == null)
+            {
+                return NotFound();
+            }
+
+            family.Name = familyDTO.Name;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException) when (!FamilyExist(id))
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            Family? family = await _context.Families.FindAsync(id);
+            if (family == null)
+            {
+                return NotFound();
+            }
+
+            if (family.Items != null && family.Items.Length > 0)
+            {
+                return Forbid();
+            }
+
+            _context.Families.Remove(family);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool FamilyExist(long id)
+        {
+            return _context.Families.Any((Family family) => family.Id == id);
+        }
+
+        /**
+         * Caste une famille en DTO
+         * 
+         * @param family Famille à caster
+         * @return Famille DTO
+         */
+        internal static FamilyDTO ConvertToFamilyDTO(Family? family)
         {
             FamilyDTO familyDTO = new FamilyDTO();
-            familyDTO.Id = family.Id;
-            familyDTO.Name = family.Name;
+
+            if (family != null)
+            {
+                familyDTO.Id = family.Id;
+                familyDTO.Name = family.Name;
+                familyDTO.Items = family.Items?.Select((Item item) => ItemController.ConvertItemToDTO(item)).ToArray();
+            }
 
             return familyDTO;
-        }
-
-        [HttpPost]
-        public IActionResult Post([FromBody] FamilyDTO familyDTO)
-        {
-            using (NegosudContext context = new NegosudContext())
-            {
-                Family familyResult = new Family();
-
-                familyResult.Name = familyDTO.Name;
-
-                context.Families.Add(familyResult);
-                context.SaveChanges();
-                return Ok();
-            }
         }
     }
 }
