d86f59447f9436bf68615976365e8115dbeaf9ea
Renaud Levasseur <renaud.levasseur@risques-developpement.fr>
2023-01-04 15:53:52 +0100

    creation client ok

--- Negosud/Negosud.WinForm/FormCustomer.cs
+++ Negosud/Negosud.WinForm/FormCustomer.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Negosud.webapi.Models;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -17,8 +19,47 @@ namespace Negosud.WinForm
             InitializeComponent();
         }
 
-        private void ButtonAddNewCustomer_Click(object sender, EventArgs e)
+        private async void ButtonAddNewCustomer_Click(object sender, EventArgs e)
         {
+            // 1 Récupérer les valeurs de(s) textBox
+            string lastName = TextBoxCustName.Text;
+            string firstName = TextBoxCustFirstName.Text;
+            string email = TextBoxCustEmail.Text;
+            string phoneNumber = TextBoxCustPhoneNumber.Text;
+            string address = TextBoxCustAdress.Text;
+            int postalCode = Int32.Parse(TextBoxCustZipCode.Text);
+            string town = TextBoxCustCity.Text;
+
+            // 2 Créer l'objet DTO
+            CustomerDTO CustomerResult = new CustomerDTO();
+            // 3 Remplir ce nouvel Objet avec les valeurs de Etape 1
+            CustomerResult.LastName = lastName;
+            CustomerResult.FirstName = firstName;
+            CustomerResult.Email = email;
+            CustomerResult.PhoneNumber = phoneNumber;
+            CustomerResult.PhysicalAddress = address;
+            CustomerResult.PostalCode = postalCode;
+            CustomerResult.Town = town;
+            // 4 Appeler la web API (route Families et avec Post)  avec cet objet DTO
+            //Déclaration du client http
+            HttpClient httpClient = new HttpClient();
+
+            //Adresse de l'api 
+            httpClient.BaseAddress = new Uri("https://localhost:7049/");
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, "customers");
+            // Sérialiser le DTO
+            string JSon = JsonConvert.SerializeObject(CustomerResult);
+            request.Content = new StringContent(JSon);
+            request.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
+            //Envoi de la requête 
+            HttpResponseMessage httpResponseMessage = await httpClient.SendAsync(request);
+            // vérifie que le retour ne soit pas une erreur
+            httpResponseMessage.EnsureSuccessStatusCode();
+            //Réponse avec message de la requête
+            string response = await httpResponseMessage.Content.ReadAsStringAsync();
+
+            //Affichage de la réponse
+            MessageBox.Show(response);
 
         }
 
