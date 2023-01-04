58701e72309144ef6605ee9167f6ef194955427e
Clément Cantaloube <clement.cantaloube@gmail.com>
2023-01-04 15:39:24 +0100

    Foreign keys added for mid tables (they were forgotten)

--- Negosud/Negosud.webapi/Models/StockMovementDTO.cs
+++ Negosud/Negosud.webapi/Models/StockMovementDTO.cs
@@ -4,7 +4,9 @@
     {
         public int Id { get; set; }
         public int Quantity { get; set; }
-        public List<ItemDTO>? Items { get; set; }
-        public List<RegularizationDTO>? Regularizations { get; set; }
+        public int ItemId { get; set; }
+        public ItemDTO Item { get; set; }
+        public int RegularizationId { get; set; }
+        public RegularizationDTO Regularization { get; set; }
     }
 }
