56c3fe9c93ae19fdf4469af82241f7dcfea1004a
Arnaud Bonduelle <arnaud.bonduelle@viacesi.fr>
2023-01-26 21:57:24 +0100

    Fond Inventaire Commandes Clients et Fournisseurs

--- Negosud/Negosud.WinForm/FormInventory.Designer.cs
+++ Negosud/Negosud.WinForm/FormInventory.Designer.cs
@@ -28,12 +28,198 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormInventory));
+            this.ButtonFamilies = new System.Windows.Forms.Button();
+            this.ButtonItems = new System.Windows.Forms.Button();
+            this.TextBoxProducts = new System.Windows.Forms.TextBox();
+            this.TextBoxMapStocks = new System.Windows.Forms.TextBox();
+            this.BtnRegularisation = new System.Windows.Forms.Button();
+            this.BtnCustOrders = new System.Windows.Forms.Button();
+            this.BtnCustomers = new System.Windows.Forms.Button();
+            this.BtnSupOrders = new System.Windows.Forms.Button();
+            this.BtnSuppliers = new System.Windows.Forms.Button();
+            this.textBoxStocks = new System.Windows.Forms.TextBox();
+            this.textBoxVentes = new System.Windows.Forms.TextBox();
+            this.textBoxAchats = new System.Windows.Forms.TextBox();
+            this.ButtonHomePage = new System.Windows.Forms.Button();
+            this.TextBoxMapInventory = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // ButtonFamilies
+            // 
+            this.ButtonFamilies.Location = new System.Drawing.Point(22, 155);
+            this.ButtonFamilies.Name = "ButtonFamilies";
+            this.ButtonFamilies.Size = new System.Drawing.Size(90, 23);
+            this.ButtonFamilies.TabIndex = 59;
+            this.ButtonFamilies.Text = "Familles";
+            this.ButtonFamilies.UseVisualStyleBackColor = true;
+            // 
+            // ButtonItems
+            // 
+            this.ButtonItems.Location = new System.Drawing.Point(22, 125);
+            this.ButtonItems.Name = "ButtonItems";
+            this.ButtonItems.Size = new System.Drawing.Size(90, 23);
+            this.ButtonItems.TabIndex = 58;
+            this.ButtonItems.Text = "Articles";
+            this.ButtonItems.UseVisualStyleBackColor = true;
+            // 
+            // TextBoxProducts
+            // 
+            this.TextBoxProducts.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TextBoxProducts.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.TextBoxProducts.Location = new System.Drawing.Point(12, 100);
+            this.TextBoxProducts.Name = "TextBoxProducts";
+            this.TextBoxProducts.Size = new System.Drawing.Size(100, 22);
+            this.TextBoxProducts.TabIndex = 57;
+            this.TextBoxProducts.Text = "PRODUITS";
+            // 
+            // TextBoxMapStocks
+            // 
+            this.TextBoxMapStocks.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TextBoxMapStocks.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.TextBoxMapStocks.Location = new System.Drawing.Point(152, 12);
+            this.TextBoxMapStocks.Name = "TextBoxMapStocks";
+            this.TextBoxMapStocks.Size = new System.Drawing.Size(100, 22);
+            this.TextBoxMapStocks.TabIndex = 56;
+            this.TextBoxMapStocks.Text = "STOCKS";
+            // 
+            // BtnRegularisation
+            // 
+            this.BtnRegularisation.Location = new System.Drawing.Point(22, 382);
+            this.BtnRegularisation.Name = "BtnRegularisation";
+            this.BtnRegularisation.Size = new System.Drawing.Size(90, 23);
+            this.BtnRegularisation.TabIndex = 54;
+            this.BtnRegularisation.Text = "Régularisation";
+            this.BtnRegularisation.UseVisualStyleBackColor = true;
+            // 
+            // BtnCustOrders
+            // 
+            this.BtnCustOrders.Location = new System.Drawing.Point(22, 325);
+            this.BtnCustOrders.Name = "BtnCustOrders";
+            this.BtnCustOrders.Size = new System.Drawing.Size(90, 23);
+            this.BtnCustOrders.TabIndex = 53;
+            this.BtnCustOrders.Text = "Commandes";
+            this.BtnCustOrders.UseVisualStyleBackColor = true;
+            // 
+            // BtnCustomers
+            // 
+            this.BtnCustomers.Location = new System.Drawing.Point(22, 297);
+            this.BtnCustomers.Name = "BtnCustomers";
+            this.BtnCustomers.Size = new System.Drawing.Size(90, 23);
+            this.BtnCustomers.TabIndex = 52;
+            this.BtnCustomers.Text = "Clients";
+            this.BtnCustomers.UseVisualStyleBackColor = true;
+            // 
+            // BtnSupOrders
+            // 
+            this.BtnSupOrders.Location = new System.Drawing.Point(22, 241);
+            this.BtnSupOrders.Name = "BtnSupOrders";
+            this.BtnSupOrders.Size = new System.Drawing.Size(90, 23);
+            this.BtnSupOrders.TabIndex = 51;
+            this.BtnSupOrders.Text = "Commandes";
+            this.BtnSupOrders.UseVisualStyleBackColor = true;
+            // 
+            // BtnSuppliers
+            // 
+            this.BtnSuppliers.Location = new System.Drawing.Point(22, 212);
+            this.BtnSuppliers.Name = "BtnSuppliers";
+            this.BtnSuppliers.Size = new System.Drawing.Size(90, 23);
+            this.BtnSuppliers.TabIndex = 50;
+            this.BtnSuppliers.Text = "Fournisseurs";
+            this.BtnSuppliers.UseVisualStyleBackColor = true;
+            // 
+            // textBoxStocks
+            // 
+            this.textBoxStocks.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxStocks.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.textBoxStocks.Location = new System.Drawing.Point(12, 354);
+            this.textBoxStocks.Name = "textBoxStocks";
+            this.textBoxStocks.Size = new System.Drawing.Size(100, 22);
+            this.textBoxStocks.TabIndex = 49;
+            this.textBoxStocks.Text = "STOCKS";
+            // 
+            // textBoxVentes
+            // 
+            this.textBoxVentes.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxVentes.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.textBoxVentes.Location = new System.Drawing.Point(12, 269);
+            this.textBoxVentes.Name = "textBoxVentes";
+            this.textBoxVentes.Size = new System.Drawing.Size(100, 22);
+            this.textBoxVentes.TabIndex = 48;
+            this.textBoxVentes.Text = "VENTES";
+            // 
+            // textBoxAchats
+            // 
+            this.textBoxAchats.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxAchats.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.textBoxAchats.Location = new System.Drawing.Point(12, 184);
+            this.textBoxAchats.Name = "textBoxAchats";
+            this.textBoxAchats.Size = new System.Drawing.Size(100, 22);
+            this.textBoxAchats.TabIndex = 47;
+            this.textBoxAchats.Text = "ACHATS";
+            // 
+            // ButtonHomePage
+            // 
+            this.ButtonHomePage.Location = new System.Drawing.Point(22, 71);
+            this.ButtonHomePage.Name = "ButtonHomePage";
+            this.ButtonHomePage.Size = new System.Drawing.Size(90, 23);
+            this.ButtonHomePage.TabIndex = 76;
+            this.ButtonHomePage.Text = "Accueil";
+            this.ButtonHomePage.UseVisualStyleBackColor = true;
+            // 
+            // TextBoxMapInventory
+            // 
+            this.TextBoxMapInventory.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TextBoxMapInventory.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.TextBoxMapInventory.ForeColor = System.Drawing.Color.SeaGreen;
+            this.TextBoxMapInventory.Location = new System.Drawing.Point(310, 5);
+            this.TextBoxMapInventory.Name = "TextBoxMapInventory";
+            this.TextBoxMapInventory.Size = new System.Drawing.Size(311, 28);
+            this.TextBoxMapInventory.TabIndex = 77;
+            this.TextBoxMapInventory.Text = "INVENTAIRE";
+            // 
+            // FormInventory
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TextBoxMapInventory);
+            this.Controls.Add(this.ButtonHomePage);
+            this.Controls.Add(this.ButtonFamilies);
+            this.Controls.Add(this.ButtonItems);
+            this.Controls.Add(this.TextBoxProducts);
+            this.Controls.Add(this.TextBoxMapStocks);
+            this.Controls.Add(this.BtnRegularisation);
+            this.Controls.Add(this.BtnCustOrders);
+            this.Controls.Add(this.BtnCustomers);
+            this.Controls.Add(this.BtnSupOrders);
+            this.Controls.Add(this.BtnSuppliers);
+            this.Controls.Add(this.textBoxStocks);
+            this.Controls.Add(this.textBoxVentes);
+            this.Controls.Add(this.textBoxAchats);
+            this.Name = "FormInventory";
             this.Text = "FormInventory";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Button ButtonFamilies;
+        private Button ButtonItems;
+        private TextBox TextBoxProducts;
+        private TextBox TextBoxMapStocks;
+        private Button BtnRegularisation;
+        private Button BtnCustOrders;
+        private Button BtnCustomers;
+        private Button BtnSupOrders;
+        private Button BtnSuppliers;
+        private TextBox textBoxStocks;
+        private TextBox textBoxVentes;
+        private TextBox textBoxAchats;
+        private Button ButtonHomePage;
+        private TextBox TextBoxMapInventory;
     }
 }
