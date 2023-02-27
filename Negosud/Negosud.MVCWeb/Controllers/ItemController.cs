7cf9d28f0acfe8ba19991f508f627bd514b7577a
Clément Cantaloube <clement.cantaloube@gmail.com>
2023-02-27 14:55:38 +0100

    Adding web app for customers

---
+++ Negosud/Negosud.MVCWeb/Controllers/ItemController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Negosud.MVCWeb.Controllers
+{
+	public class ItemController : Controller
+	{
+		public IActionResult Index()
+		{
+			ViewBag.toto = "toto";
+			return View();
+		}
+	}
+}
