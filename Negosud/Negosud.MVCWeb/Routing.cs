92f6b409a276b0f083592a4d5a17177f2ad3b552
Clément Cantaloube <clement.cantaloube@gmail.com>
2023-02-27 14:58:14 +0100

    Forgotten elem

---
+++ Negosud/Negosud.MVCWeb/Routing.cs
@@ -0,0 +1,18 @@
+﻿namespace Negosud.MVCWeb
+{
+	public class Routing
+	{
+		public static void GenerateRoutes(WebApplication app)
+		{
+			app.MapControllerRoute(
+				name: "default",
+				pattern: "{controller=Home}/{action=Index}/{id?}"
+			);
+
+			app.MapControllerRoute(
+				name: "Item",
+				pattern: "{controller=Item}/{action=Index}/{id?}"
+			);
+		}
+	}
+}
