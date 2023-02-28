f3f5a0153e1862a9f9e7bd9743e8fd8f59a56147
Clément Cantaloube <clement.cantaloube@gmail.com>
2023-02-28 10:45:35 +0100

    Add missing types

---
+++ Negosud/Negosud.MVCWeb/Types/ReasonRegularization.cs
@@ -0,0 +1,10 @@
+﻿namespace Negosud.MVCWeb.Types
+{
+    public class ReasonRegularization
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = "";
+        public bool Sense { get; set; }
+        public List<Regularization>? Regularizations { get; set; }
+    }
+}
