2d2ac59f1cf46cd97d9be36380075c6325ea0139
Clément Cantaloube <clement.cantaloube@gmail.com>
2023-02-28 14:32:11 +0100

    Add BasketController

---
+++ Negosud/Negosud.MVCWeb/Controllers/BasketController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Negosud.MVCWeb.Controllers
+{
+    public class BasketController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
