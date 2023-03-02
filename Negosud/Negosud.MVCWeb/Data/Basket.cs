c6cb304a21afcea817e16c07fd80730423de1ab6
Clément Cantaloube <clement.cantaloube@gmail.com>
2023-03-02 08:59:39 +0100

    Add items in basket (WIP)

---
+++ Negosud/Negosud.MVCWeb/Data/Basket.cs
@@ -0,0 +1,9 @@
+﻿namespace Negosud.MVCWeb.Data
+{
+    public class Basket
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+    }
+}
