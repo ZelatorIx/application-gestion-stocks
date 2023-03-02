206dc474130eee39c5134b137d021bcd23a6ffba
Clément Cantaloube <clement.cantaloube@gmail.com>
2023-03-02 16:35:36 +0100

    Research functionality (WIP)

--- Negosud/Negosud.MVCWeb/Controllers/ItemController.cs
+++ Negosud/Negosud.MVCWeb/Controllers/ItemController.cs
@@ -7,28 +7,41 @@ namespace Negosud.MVCWeb.Controllers
     public class ItemController : Controller
 	{
 		private readonly Model model;
-        private readonly ShoppingCartController basket;
 
         public ItemController(Model model, ShoppingCartController basket)
 		{
 			this.model = model;
-			this.basket = basket;
 		}
 
-		[Route("Item")]
-        public async Task<ViewResult> Index()
+        public async Task<IActionResult> Index()
 		{
 			List<Item> items = await model.GetItems();
+			List<Family> families = await model.GetFamilies();
 
-			return View(items);
+			return View(new { items, families });
         }
 
-        [Route("Item/{id}")]
-		public async Task<ViewResult> Details(int id)
+		public async Task<IActionResult> Details(int id)
 		{
             Item? item = await model.GetItemById(id);
 
             return View(item);
         }
+
+		public async Task<IActionResult> Search([FromForm] string form)
+		{
+			List<Item> allItems = await model.GetItems();
+			List<Family> allFamilies = await model.GetFamilies();
+
+            int familyId = 0;
+			string research = "";
+
+			List<Item> items = allItems
+				.Where((Item item, int index) => item.FamilyId == familyId)
+				.Where((Item item, int index) => allItems[index].Name.Contains(research))
+				.ToList();
+
+            return View(new { items, families = allFamilies });
+		}
 	}
 }
